a955aad3bf57eb8a9de9b5700015c4f678c20f07
ValeryTychkin <valery.tychkin@gmail.com>
2024-04-04 22:53:01 +0300

    add keymaps for mac and neovidels

--- init.lua
+++ init.lua
@@ -1,3 +1,23 @@
+if vim.g.neovide then
+  vim.g.neovide_cursor_trail_size = 0.5
+  vim.g.neovide_cursor_animation_length = 0.07
+  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_input_use_logo = 1
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
+  vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+
 return {
   -- Configure AstroNvim updates
   updater = {
