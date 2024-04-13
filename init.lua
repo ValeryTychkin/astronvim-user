0d201bd9ce2dab3a15189e794dc5c20acde1412f
ValeryTychkin <valery.tychkin@gmail.com>
2024-04-13 13:32:07 +0300

    Add scrollbar and lsp signature

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ if vim.g.neovide then
   vim.g.neovide_input_use_logo = 1
   vim.g.neovide_hide_mouse_when_typing = true
 
-  vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
   vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
   vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
   vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
@@ -13,10 +12,6 @@ if vim.g.neovide then
   vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
 end
 
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 
 return {
   -- Configure AstroNvim updates
--- plugins/user.lua
+++ plugins/user.lua
@@ -2,13 +2,6 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
   "ValeryTychkin/xcode-dark.nvim",
 
   {
@@ -16,5 +9,53 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
+  },
+
+  {
+    "petertriho/nvim-scrollbar",
+    opts = function(_, opts)
+      require("astrocore").extend_tbl(opts, {
+        handlers = {
+          gitsigns = require("astrocore").is_available "gitsigns.nvim",
+          search = require("astrocore").is_available "nvim-hlslens",
+          ale = require("astrocore").is_available "ale",
+        },
+      })
+    end,
+    event = "User AstroFile",
+    config = function ()
+      require("scrollbar").setup({
+            handle = {
+            color = "#2E323C",
+        },
+            marks = {
+            Search = { color = "#FF8700" },
+            Error = { color = "#e95678" },
+            Warn = { color = "#f7bb3b" },
+            Info = { color = "#61afef" },
+            Hint = { color = "#36d0e0" },
+            Misc = { color = "#FFFFFF" },
+          }
+      })
+    end
+  },
+
+  "nvimdev/zephyr-nvim",
+
+  {
+    "AstroNvim/astrocore",
+    lazy = false, -- disable lazy loading
+    priority = 10000, -- load AstroCore first
+    opts = {
+        -- set configuration options  as described below
+    }
+  },
+
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
   },
 }
